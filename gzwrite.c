b16aaadc47171026e4001c0fedc803ebb5c792a9
xinking129 <xinxin13@huawei.com>
2024-04-17 16:58:06 +0800

    adapt old version
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- gzwrite.c
+++ gzwrite.c
@@ -4,6 +4,8 @@
  */
 
 #include "gzguts.h"
+#include <sys/types.h>
+#include <unistd.h>
 
 /* Initialize state for writing a gzip file.  Mark initialization by setting
    state->size to non-zero.  Return -1 on a memory allocation failure, or 0 on
